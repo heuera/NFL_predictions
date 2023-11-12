78a7bfc66b207585328235f55c4c12c16f372614
heuera <austinheuer24@gmail.com>
2023-11-12 12:50:59 -0500

    Added missing rbind to merge scipt

--- merge_football_ref_data.R
+++ merge_football_ref_data.R
@@ -3,12 +3,12 @@ library(tidyverse)
 library(readxl)
 
 # Read in data
-p1 <- read_xlsx("~/Desktop/NFL_predictions/sportsref_download (2).xlsx", skip = 1)
-p2 <- read_xlsx("~/Desktop/NFL_predictions/sportsref_download (3).xlsx", skip = 1)
-p3 <- read_xlsx("~/Desktop/NFL_predictions/sportsref_download (4).xlsx", skip = 1)
+d1 <- read_xlsx("~/Desktop/NFL_predictions/sportsref_download (2).xlsx", skip = 1)
+d2 <- read_xlsx("~/Desktop/NFL_predictions/sportsref_download (3).xlsx", skip = 1)
+d3 <- read_xlsx("~/Desktop/NFL_predictions/sportsref_download (4).xlsx", skip = 1)
 
 # Add data frames to list
-data_frames <- list(p1, p2, p3)
+data_frames <- list(d1, d2, d3)
 
 # Function to check if first row is NA and remove if true
 remove_empty_first_row <- function(df) {
@@ -28,9 +28,12 @@ data_frames <- lapply(data_frames, remove_empty_first_row)
 
 # Loop through each data frame and assign it to the original data frame
 for (i in seq_along(data_frames)) {
-        assign(paste("p", i, sep = ""), data_frames[[i]])
+        assign(paste("d", i, sep = ""), data_frames[[i]])
 }
 
+# Bind data frames into one data frame
+rushing <- do.call(rbind, data_frames)
+
 # Write the combined data frame to a CSV file
-write.csv(passing, "passing.csv", row.names = TRUE)
+write.csv(rushing, "rushing.csv", row.names = TRUE)
 
